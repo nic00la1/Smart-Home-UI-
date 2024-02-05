55170a93ac9e7e57e3cc2fc59360c94e3a0a125e
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-05 21:34:01 +0100

    Background and Title ✅

--- ContentView.swift
+++ ContentView.swift
@@ -15,9 +15,39 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
+            AnimatedBackground()
+            Title()
             
         }
         .ignoresSafeArea()
+    }
+    
+    @ViewBuilder
+    func Title() -> some View {
+        VStack {
+            VStack(alignment: .leading, content: {
+                HStack {
+                    Text("BEDROM")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 28))
+                        .bold()
+                    
+                    Spacer()
+                }
+                .padding(.bottom, 10)
+                
+                HStack {
+                    Text("TOTAL 4 ACTIVE DEVICES")
+                        .foregroundStyle(.yellow)
+                        .font(.system(size: 10))
+                        .bold()
+                    
+                    Spacer()
+                }
+            })
+        }
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+        .padding(.horizontal)
     }
 }
 
